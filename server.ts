885553eb56ac28194677949ea9da68e5d2237be2
Louis Orleans <louis@orleans.io>
2023-07-19 11:17:29 -0700

    ✨ improve server usability

--- server.ts
+++ server.ts
@@ -4,11 +4,13 @@ import { launch_kml_gen } from './main.ts';
 const app = new Application();
 const router = new Router();
 
+router.redirect('/', 'launch_kml_gen', 307);
+
 router.get('/launch_kml_gen/:fc_mis_id', async (ctx) => {
   const fc_mis_id = ctx.request.url.pathname.split('/').at(-1);
 
   if (fc_mis_id == null) {
-    ctx.response.body = `missing fc_uuid`;
+    ctx.response.body = `missing fc_mis_id`;
     ctx.response.status = 404;
     return;
   }
@@ -31,24 +33,10 @@ router.get('/launch_kml_gen', async (ctx) => {
   const res = await fetch(`https://api.flightclub.io/v3/mission/next`);
   const fc_mis_id = (await res.json()).resourceId;
 
-  if (fc_mis_id == null) {
-    ctx.response.body = `missing fc_uuid`;
-    ctx.response.status = 500;
-    return;
-  }
-
-  try {
-    ctx.response.body = (await launch_kml_gen(fc_mis_id)).kml_contents;
-    ctx.response.headers.set(
-      'Content-Type',
-      'application/vnd.google-earth.kml+xml',
-    );
-    return;
-  } catch (e) {
-    ctx.response.body = e.message;
-    ctx.response.status = 500;
-    return;
-  }
+  ctx.response.body = `Getting next launch: ${fc_mis_id}`;
+  ctx.response.status = 307;
+  ctx.response.redirect(`/launch_kml_gen/${fc_mis_id}`);
+  return;
 });
 
 app.use(router.routes());
